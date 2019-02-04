995ea97d07ff346466167265a429cb728520a9c4
Chance Smith <chancesmith97@hotmail.com>
2019-02-04 18:12:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,6 +12,6 @@ end
 def take_a_number(x,y)
 x.push(y)
 place_in_line = "Welcome, #{y}. You are number"
-x.map.with_index {z,i} place_in_line <<"#{i + 1} in line."
+x.map.with_index {i} place_in_line <<"#{i + 1} in line."
 puts place_in_line
 end
